a6435631b7e532ba58d3ae4ec1c0a7c300f39cd9
adamacosta <acosta.adam@gmail.com>
2015-06-13 12:26:29 -0500

    added switch to choose aos tests

--- aos/bvt/README.md
+++ aos/bvt/README.md
@@ -1,8 +1,20 @@
-# CS 6210 Advanced Operating Systems
+CS 6210 Advanced Operating Systems
+==================================
 
 This module comprises the build verification tester for CS 6210. Currently, only 
 the tests for assignment 4, Recoverable Virtual Memory, are complete. Other tests 
 are commented out.
 
-To run tests, edit the bvt.sh export directives to point to your own assignment 
-directories, then execute `./bvt.sh`.
+Usage
+=====
+
+The bvt.sh script contains four environment variable export statements, telling 
+each Makefile where to find your assignment deliverables in your home directory. 
+There is no need to edit the Makefiles. Simply edit the export directives to 
+point to the correct directories on your machine, and then execute:
+
+```bash
+./bvt.sh [gtthreads|barrier|proxy-server|rvm|all]
+```
+
+to test any individual build, or all of them. 
--- aos/bvt/bvt.sh
+++ aos/bvt/bvt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export GTTHREADDIR=$HOME/omscs/aos/gtthreads
-export BARRIERDIR=$HOME/omscs/aos/barrier 
-export PROXYDIR=$HOME/omscs/aos/proxy-server
-export RVMDIR=$HOME/omscs/aos/rvm
+if [ -z $1 ]; then
+	echo "usage: bvt.sh [gtthreads|barrier|proxy-server|rvm|all]"
+	exit
+fi
+
 PYTEST=`which pytest`
 
 if [ "$PYTEST" = "" ]; then
@@ -11,42 +12,64 @@ if [ "$PYTEST" = "" ]; then
 	exit
 fi
 
-echo "=============================== build gtthreads ================================"
-#pushd gtthreads
-#make tests > /dev/null
-#pushd tests > /dev/null
-#py.test -s -v test_gtthreads.py
-#popd > /dev/null
-#make clean > /dev/null
-#popd
-echo "============================== gtthreads complete =============================="
+export GTTHREADDIR=$HOME/omscs/aos/gtthreads
+export BARRIERDIR=$HOME/omscs/aos/barrier 
+export PROXYDIR=$HOME/omscs/aos/proxy-server
+export RVMDIR=$HOME/omscs/aos/rvm
 
-echo "================================ build barrier ================================="
-#pushd barrier
-#make tests > /dev/null
-#pushd tests > /dev/null
-#py.test -s -v test_barrier.py
-#popd > /dev/null
-#make clean > /dev/null
-#popd
-echo "=============================== barrier complete ==============================="
+function test_gtthreads {
+	echo "=============================== build gtthreads ================================"
+	#pushd gtthreads
+	#make tests > /dev/null
+	#pushd tests > /dev/null
+	#py.test -s -v test_gtthreads.py
+	#popd > /dev/null
+	#make clean > /dev/null
+	#popd
+	echo "============================== gtthreads complete =============================="
+}
 
-echo "============================== build proxy-server =============================="
-#pushd proxy-server
-#make tests > /dev/null
-#pushd tests > /dev/null
-#py.test -s -v test_proxy-server.py
-#popd > /dev/null
-#make clean > /dev/null
-#popd
-echo "============================ proxy-server complete ============================="
+function test_barrier {
+	echo "================================ build barrier ================================="
+	#pushd barrier
+	#make tests > /dev/null
+	#pushd tests > /dev/null
+	#py.test -s -v test_barrier.py
+	#popd > /dev/null
+	#make clean > /dev/null
+	#popd
+	echo "=============================== barrier complete ==============================="
+}
 
-echo "================================== build rvm ==================================="
-pushd rvm > /dev/null
-make tests > /dev/null
-pushd tests > /dev/null
-py.test -s -v test_rvm.py
-popd > /dev/null
-make clean > /dev/null
-popd > /dev/null
-echo "================================ rvm complete =================================="
+function test_proxy-server {
+	echo "============================== build proxy-server =============================="
+	#pushd proxy-server
+	#make tests > /dev/null
+	#pushd tests > /dev/null
+	#py.test -s -v test_proxy-server.py
+	#popd > /dev/null
+	#make clean > /dev/null
+	#popd
+	echo "============================ proxy-server complete ============================="
+}
+
+function test_rvm {
+	echo "================================== build rvm ==================================="
+	pushd rvm > /dev/null
+	make tests > /dev/null
+	pushd tests > /dev/null
+	py.test -s -v test_rvm.py
+	popd > /dev/null
+	make clean > /dev/null
+	popd > /dev/null
+	echo "================================ rvm complete =================================="
+}
+
+function test_all {
+	test_gtthreads
+	test_barrier
+	test_proxy-server
+	test_rvm
+}
+
+test_$1
